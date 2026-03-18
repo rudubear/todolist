0d4fd05201922cdda461a4e97bb5be4238c54aed
RuduBear <134881527+rudubear@users.noreply.github.com>
2026-03-18 14:50:54 -0400

    initial set up for todolist

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 // index.js
 import "./styles.css";
 
-import img_restaurant_bg from "../assets/images/restaurant_background.png";
+import img_restaurant_bg from "../assets/images/todolist_background.jpg";
 
-import { page_load_text_home, div_home } from "./home.js";
-import { page_load_text_menu, div_menu} from "./menu.js";
-import { page_load_text_about, div_aboutus } from "./about.js";
+import { page_load_text_home } from "./home.js";
+import { page_load_text_menu } from "./menu.js";
+import { page_load_text_about } from "./about.js";
 import { page_HTMLRenderer } from "./renderHTML.js";
 
 console.log(page_load_text_about);
@@ -15,8 +15,6 @@ console.log(page_HTMLRenderer);
 
 let divContent = document.getElementById("content");
 let divBody = document.querySelector('body');
-
-divContent.appendChild(div_home);
 
 const image_restaurant_bg = document.createElement("img");
 image_restaurant_bg.src = img_restaurant_bg;
@@ -30,18 +28,15 @@ const btn_about = document.getElementById("btn_About");
 btn_home.addEventListener("click", (event) => {
     console.log("home button clicked");
     divContent.replaceChildren();
-    divContent.appendChild(div_home);
 
 });
 
 btn_menu.addEventListener("click", (event) => {
     console.log("menu button clicked");
     divContent.replaceChildren();
-    divContent.appendChild(div_menu);
 });
 
 btn_about.addEventListener("click", (event) => {
     console.log("about button clicked");
     divContent.replaceChildren();
-    divContent.appendChild(div_aboutus);
 });
